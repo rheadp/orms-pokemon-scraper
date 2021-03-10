c669df0d15ad6c8d4f70d24524a18e95584118bf
rheadp <rheadpea@msu.edu>
2021-03-10 12:38:12 -0500

    commit tree 6a933472ec40807ced7ff63c227297df5f8fb2df

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
-end
+    attr_accessor :id, :name, :type, :db
+    def initialize(id=nil, name=nil, type=nil, db=nil)
+      @id = id
+      @name = name
+      @type = type
+      @db = db
+    end
+  
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon(name, type) VALUES (?, ?)", name, type)
+      id = db.execute("SELECT last_insert_rowid() FROM pokemon")
+      self.new(id,name,type,db)
+    end
+  
+    def self.find(id, db)
+      pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)[0]
+      id, name, type = pokemon
+      self.new(id, name, type, db)
+    end
+  end
